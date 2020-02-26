8d2decf98d6600fd7ee4a41b208017c00530a4ef
Liangding Li <liangdingli@Liangdings-MacBook-Pro.local>
2020-02-26 12:13:07 -0500

    2.26

--- src/CacheSimulater.java
+++ src/CacheSimulater.java
@@ -4,6 +4,20 @@
 public class CacheSimulater {
     Cache l1Cache;
     Cache l2Cache;
+    //Simulater states
+    Integer l1Reads = 0;
+    Integer l1ReadMiss = 0;
+    Integer l1Writes = 0;
+    Integer l1WriteMiss = 0;
+    Integer l1Writebacks = 0;
+    Double  l1MissRate = 0.0;
+    Integer l2Reads = 0;
+    Integer l2ReadMiss = 0;
+    Integer l2Writes = 0;
+    Integer l2WriteMiss = 0;
+    Integer l2Writebacks = 0;
+    Double  l2MissRate = 0.0;
+
     
     public CacheSimulater(Integer blockSize, Integer l1Size, Integer l1Assoc, Integer l2Size, Integer l2Assoc, Integer replacementPolicy, Integer inclusionProperty) {
         CacheFactory cacheFactory = new CacheFactory();
@@ -23,52 +37,98 @@ public class CacheSimulater {
     public void runSimulationByStep(String operation) {
         Long address = getAddress(operation);
         Integer operand = getOperand(operation);
+
+        if (operand == 0) {
+            read(address);
+        } else {
+            write(address);
+        }
         // Check whether L1 cache hit
-        if (l1Cache.isHit(address)) {  //increment hit count in cache itself
-            if (operand == 0) {
-                l1Cache.read(address);
-            } else {
-                l1Cache.write(address);  // TODO: handle mark  dirty in write
-            }
-        } else { // If not hit
-                // Handle L2 first
-            if (l2Cache.isHit(address)) {
-                if (operand == 0) {
+        // if (l1Cache.isHit(address)) {  //increment hit count in cache itself
+        //     if (operand == 0) {
+        //         l1Cache.read(address);
+        //     } else {
+        //         l1Cache.write(address);  // TODO: handle mark dirty in write
+        //     }
+        // } else { // If not hit
+        //     // Handle L2 first
+        //     if (l2Cache != null) {
+        //         if (l2Cache.isHit(address)) {
+        //             if (operand == 0) {
+        //                 l2Cache.read(address);
+        //             } else {
+        //                 l2Cache.write(address);
+        //             }
+        //         } else {
+        //             Long l2Evicted = l2Cache.evict(address);
+        //             l2Cache.write(address);
+        //             if (operand == 0) {
+        //                 l2Cache.read(address);       
+        //             }            
+        //         }
+        //     }
+        //     Long l1Evicted = l1Cache.evict(address);
+        //     l1Cache.write(address);
+        //     if (operand == 0) {
+        //         l1Cache.read(address);       
+        //     } 
+
+        //     if (l1Evicted != null) {
+        //         if (l2Cache != null) {
+        //             if (l2Cache.isHit(l1Evicted)) {
+        //                 if (operand == 0) {
+        //                     l2Cache.read(l1Evicted);
+        //                 } else {
+        //                     l2Cache.write(l1Evicted); //Should mark dirty
+        //                 }
+        //             } else {
+        //                 Long l2Evicted = l2Cache.evict(address);
+        //                 l2Cache.write(l1Evicted);
+        //                 if (operand == 0) {
+        //                     l2Cache.read(l1Evicted);       
+        //                 }            
+        //             }
+        //         }   
+        //     }
+        // }
+        // return;
+    }
+
+    public void read(Long address) {
+        if (l1Cache.isHit(address)) {
+            l1Cache.read(address); //update 
+            return;
+        } else {
+            Long l1Evict = l1Cache.evict(address);
+            if (l2Cache != null) {
+                if (l1Evict != null) {
+                    //Init a l2 write
+                    if (l2Cache.isHit(l1Evict)) {
+                        l2Cache.writeAndSetDirty(l1Evict);
+                    } else { //if l2 not hit on write
+                        Long l2Evict = l2Cache.evict(l1Evict);
+                        l2Cache.writeAndSetDirty(l1Evict);
+                    }
+                //Init a l2 read
+                if (l2Cache.isHit(address)) {
                     l2Cache.read(address);
                 } else {
+                    Long l2Evict = l2Cache.evict(address);
                     l2Cache.write(address);
                 }
-            } else {
-                Long l2Evicted = l2Cache.evict(address);
-                l2Cache.write(address);
-                if (operand == 0) {
-                    l2Cache.read(address);       
-                }            
             }
-
-            Long l1Evicted = l1Cache.evict(address);
-            l1Cache.write(address);
-            if (operand == 0) {
-                l1Cache.read(address);       
-            } 
-
-            if (l1Evicted != null) {
-                if (l2Cache.isHit(l1Evicted)) {
-                    if (operand == 0) {
-                        l2Cache.read(l1Evicted);
-                    } else {
-                        l2Cache.write(l1Evicted); //Should mark dirty
-                    }
-                } else {
-                    Long l2Evicted = l2Cache.evict(address);
-                    l2Cache.write(l1Evicted);
-                    if (operand == 0) {
-                        l2Cache.read(l1Evicted);       
-                    }            
-                }
-            }
+            l1Cache.write(address); //Write here is allocate
+            l1Cache.read(address);
         }
-        return;
+    }
+
+    public void write(Long address) {
+        if (l1Cache.isHit(address)) {
+            l1Cache.writeAndSetDirty(address);
+        } else {
+            Long l1Evict = l1Cache.evict(address);
+            if (l2Cache != null && )
+        }
     }
 
     public void printStates() {
---
+++ src/FIFOCache2.java
@@ -0,0 +1,146 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+/**
+ * FIFOCache2
+ */
+public class FIFOCache2 implements Cache {
+    // Cache meta data
+    Integer size;
+    Integer assoc;
+    Integer blockSize;
+    Integer numOfSet;
+    Integer replacementPolicy;
+    Integer inclusionProperty;
+    List<LinkedList<Long>> queue;  
+    private Long tagLength;
+    private Long idxLength;
+    private Long blockLength;
+    // Cache states
+    public Integer numOfRead = 0;
+    Integer numOfReadMiss = 0;
+    Integer numOfWrite = 0;
+    Integer numOfWriteMiss = 0;
+    Double missRate = 0.0;
+    Integer numOfWriteback = 0;
+
+    public FIFOCache2(Integer inputSize, Integer inputAssoc, Integer inputBlockSize, Integer inputReplacementPolicy, Integer inputInclusionProperty) {
+        size = inputSize;
+        assoc = inputAssoc;
+        blockSize = inputBlockSize;
+        numOfSet = size / (assoc * blockSize);
+        replacementPolicy = inputReplacementPolicy;
+        inclusionProperty = inputInclusionProperty;
+        for (int i = 0; i < numOfSet; i++) { //Init cache
+            LinkedList<Long> set = new LinkedList<>();
+            for (Long j = 0L; j < assoc; j++) {
+                set.add(j << 32); // Using [32 : 32 + assoc] to store way index
+            }
+            queue.add(set); 
+        }
+        idxLength = log2(numOfSet);
+        blockLength = log2(blockSize * 8);//16 in byte
+        tagLength = 32 - idxLength - blockLength;
+    }
+
+    /**
+     * Check whether is hit; update hit and miss counter here
+     */
+    @Override
+    public Boolean isHit(Long address) {
+        // TODO Auto-generated method stub
+        Long tag = getTag(address);
+        Integer index = getIndex(address).intValue();
+        for (int i = 0; i < assoc; i++) {
+            Long curTag = (queue.get(index).get(i) >> 2) & (1L << 30 - 1L); //Last 2 bit for dirty and valid, the 32 to
+            if (curTag == tag) { 
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Read the cache line; Return the content; Do not change the order 
+     */
+    @Override
+    public Long read(Long address) {
+        // TODO update order and cache states
+        Long tag = getTag(address);
+        Integer index = getIndex(address).intValue();
+        for (int i = 0; i < assoc; i++) {
+            if ((queue.get(index).get(i) >> 3) == tag) { //Last bit for valid or invalid, the second last bit for dirty or non-dirty
+                return  queue.get(index).get(i);
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public Long write(Long address) {
+        // TODO update order and cache states
+        Long tag = getTag(address);
+        Integer index = getIndex(address).intValue();
+        // Write when hit, no need to change the block index
+        for (int i = 0; i < assoc; i++) {
+            Long curr = queue.get(index).get(i);
+            if ((curr >> 3) == tag) { 
+                queue.get(index).set(i, curr | 4L); // & 0...0000100 to set the dirty bit
+                return queue.get(index).get(i);
+            }
+        }
+        //Write when miss, need to change the block index
+        for (int i = 0; i < assoc; i++) {
+            Long curr = queue.get(index).get(i);
+            if (((curr & 4L) == 1L) || (curr >> 1 == 0L)) { 
+                queue.get(index).set(i, (tag << 3) & ); // & 0...0000100 to set the dirty bit
+                return queue.get(index).get(i);
+            }
+        }
+        return null; // Impossible in my case
+    }
+
+    @Override
+    public Long evict(Long address) {
+        Integer index = getIndex(address).intValue();
+        for (int i = 0; i < assoc; i++) {
+            if ((cacheData[index][i] & 1L) == 0 || cacheData[index][i] == 0) { 
+                return null;
+            } 
+        }
+        for (int i = 0; i < assoc; i++) {
+            // TODO choose the one with smallest order and evict it
+        }
+        return null;
+    }
+
+    @Override
+    public void printState() {
+        // TODO Auto-generated method stub
+
+    }
+
+    private static Long log2(Integer n) {
+        return Long.valueOf((Integer)(int)(Math.log(n) / Math.log(2)));
+    }
+
+    private Long getTag(Long address) {
+        return address >> (blockLength + idxLength);
+    }
+
+    private Long getIndex(Long address) {
+        return (address >> blockLength) & ((1L << idxLength) - 1L);
+    }
+
+
+
+    public static void main(String[] args) {
+        FIFOCache2 myCache = new FIFOCache2(1024, 2, 16, 0, 0);
+        System.out.println(myCache.getIndex(1073955232L));
+        System.out.println(myCache.getTag(1073955232L));
+        // System.out.println(myCache.numOfSet);
+        // System.out.println(myCache.tagLength);
+        // System.out.println(myCache.idxLength);
+        // System.out.println(myCache.blockLength);
+    }
+}
